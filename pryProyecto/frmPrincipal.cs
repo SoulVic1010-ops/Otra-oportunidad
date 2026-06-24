f6fdb2d40a04c068b1627c8ddc0158e9407dd602
rotcIVan naVIc <victordanielfranciscomendoza@gmail.com>
2026-06-23 23:04:46 -0600

    Cambios Listos

--- pryProyecto/frmPrincipal.cs
+++ pryProyecto/frmPrincipal.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -55,6 +56,12 @@ namespace pryProyecto
         {
             principal = new clsPrincipal();
             principal.agregarAlContenedor(new frmCarreras(), pnlContenedor);
+        }
+
+        private void pcbTutores_Click(object sender, EventArgs e)
+        {
+            principal = new clsPrincipal();
+            principal.agregarAlContenedor(new frmTutores(), pnlContenedor);
         }
     }
 }
