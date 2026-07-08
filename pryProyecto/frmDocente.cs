4e0b12ca6fc5b4d24578feb97228368cad02c2a5
Yahir-07 <20251168@uthh.edu.mx>
2026-07-07 21:05:45 -0600

    clsDocentes

--- pryProyecto/frmDocente.cs
+++ pryProyecto/frmDocente.cs
@@ -12,9 +12,31 @@ namespace pryProyecto
 {
 	public partial class frmDocente : Form
 	{
+		clsDocente docente;
 		public frmDocente()
 		{
 			InitializeComponent();
+			CargarGrid();
+			
+		}
+		public void CargarGrid()
+		{
+			docente = new clsDocente();
+			dgvDocente.DataSource = null;
+			dgvDocente.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+			try
+			{
+				dgvDocente.DataSource = docente.cargarDataGrid();
+
+				dgvDocente.Columns["vchpassword"].Visible = false;
+				dgvDocente.Columns["vchperfil"].Visible = false;
+				dgvDocente.Columns["idUsuario"].Visible = false;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
 		}
 	}
 }
